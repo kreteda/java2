c5df862140393c10ffe820ec3b8db88327e6e855
Rafał Hrycikowski <susxe@daintly.com>
2016-02-29 12:58:48 +0100

    dsa
    dsad

---
+++ Table.java
@@ -0,0 +1,41 @@
+package sprawdzian.sprawdzian1;
+
+public class Table {
+
+int s;
+float b ;
+double o;
+
+
+public int gets1() {
+	return s;
+}
+
+public float getb1() {
+	return b;
+}
+
+public int dodajs(int a)
+{
+	return s=s+a;
+	
+}
+
+public float dodajf(float a)
+{
+	return b=b+a;
+}
+
+public double dodajd(double a)
+{
+	return o=o+a;
+}
+
+public static void main(String[] args){
+	
+ 
+}
+
+ 
+
+}
